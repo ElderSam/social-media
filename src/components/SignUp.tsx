8b94bbb561e307fde419264c0dff7bafcc31f62c
ElderSam <eldersamuel98@gmail.com>
2026-02-09 12:33:07 -0300

    feat: enable button state based on username input in SignUp component

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,15 +1,24 @@
+import { useState } from 'react';
 import './SignUp.css';
 
 export default function SignUp() {
+  const [username, setUsername] = useState('');
+
   return (
     <div className="modal">
       <div className="modal-container">
           <p className="modal-title">Welcome to CodeLeap network!</p>
 
           <p className="input-title">Please enter your username</p>
-          <input type="text" placeholder="John doe" autoFocus/>
+          <input
+            type="text"
+            placeholder="John doe"
+            autoFocus
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
 
-          <button>
+          <button disabled={!username.trim()}>
             <p>ENTER</p>
           </button>
       </div>
