2300344643812a59eec618d1d95fa89d785bdbac
ElderSam <eldersamuel98@gmail.com>
2026-02-09 12:24:41 -0300

    feat: implement SignUp component with modal styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
+import SignUp from './components/SignUp';
 import './App.css';
 
 function App() {
   return (
-    <h1>
-      Hello Wolrd
-    </h1>
+    <SignUp />
   );
 }
 
---
+++ src/components/SignUp.tsx
@@ -0,0 +1,14 @@
+import './SignUp.css';
+
+export default function SignUp() {
+  return (
+    <div className="modal">
+      <div className="modal-container">
+          <p className="modal-title">Welcome to CodeLeap network!</p>
+
+          <p className="input-title">Please enter your username</p>
+          <input type="text" placeholder="John doe" />
+      </div>
+    </div>
+  );
+}
