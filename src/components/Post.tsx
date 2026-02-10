92d830c58418a71930ad285206f30b49a3e2767e
ElderSam <eldersamuel98@gmail.com>
2026-02-09 22:33:28 -0300

    feat: conditionally render Post actions based on ownership

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -5,6 +5,9 @@ import deleteIcon from '../assets/delete-icon.svg';
 import './Post.css';
 
 export default function Post({ post }: {post: PostType}) {
+  const currentUsername = localStorage.getItem('username');
+  const isOwnPost = currentUsername === post.username;
+
   const handleEdit = () => {
     console.log('Edit post:', post.id);
     // TODO: Implement edit functionality
@@ -19,10 +22,12 @@ export default function Post({ post }: {post: PostType}) {
     <div key={post.id} className="post-item">
       <div className="post-header">
         <h3 className="post-title">{post.title}</h3>
-        <div className="post-actions">
-          <img src={deleteIcon} alt="Delete" className="action-icon" onClick={handleDelete} />
-          <img src={editIcon} alt="Edit" className="action-icon" onClick={handleEdit} />
-        </div>
+        {isOwnPost && (
+          <div className="post-actions">
+            <img src={deleteIcon} alt="Delete" className="action-icon" onClick={handleDelete} />
+            <img src={editIcon} alt="Edit" className="action-icon" onClick={handleEdit} />
+          </div>
+        )}
       </div>
       <div className="post-meta">
         <span className="post-username">@{post.username}</span>
