b68850b4cd27bce815d89e3ec2b4618d434e6117
ElderSam <eldersamuel98@gmail.com>
2026-02-09 21:05:17 -0300

    feat: add edit and delete icons to Post component with corresponding functionality

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,12 +1,28 @@
 import type { PostType } from "../types/types";
 import { getRelativeTime } from "../utils/timeUtils";
+import editIcon from '../assets/edit-icon.svg';
+import deleteIcon from '../assets/delete-icon.svg';
 import './Post.css';
 
 export default function Post({ post }: {post: PostType}) {
+  const handleEdit = () => {
+    console.log('Edit post:', post.id);
+    // TODO: Implement edit functionality
+  };
+
+  const handleDelete = () => {
+    console.log('Delete post:', post.id);
+    // TODO: Implement delete functionality
+  };
+
   return (
     <div key={post.id} className="post-item">
       <div className="post-header">
         <h3 className="post-title">{post.title}</h3>
+        <div className="post-actions">
+          <img src={deleteIcon} alt="Delete" className="action-icon" onClick={handleDelete} />
+          <img src={editIcon} alt="Edit" className="action-icon" onClick={handleEdit} />
+        </div>
       </div>
       <div className="post-meta">
         <span className="post-username">@{post.username}</span>
